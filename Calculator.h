50e0a1fca136738fe4662d2b529b0db5b26a394a
Douglass Turner <douglass.turner@gmail.com>
2013-05-27 08:20:06 -0400

    Unit tests - test_GivenSUT_WhenUsedInUnitTest_ThenShouldNotBeNil passed.

---
+++ Calculator.h
@@ -0,0 +1,13 @@
+//
+// Created by Douglass Turner on 5/27/13.
+// Copyright (c) 2013 Elastic Image Software. All rights reserved.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface Calculator : NSObject
+@end
---
+++ Calculator.m
@@ -0,0 +1,15 @@
+//
+// Created by Douglass Turner on 5/27/13.
+// Copyright (c) 2013 Elastic Image Software. All rights reserved.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "Calculator.h"
+
+
+@implementation Calculator {
+
+}
+@end
