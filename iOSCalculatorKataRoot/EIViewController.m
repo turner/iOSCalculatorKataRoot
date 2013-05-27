472850dc72fe0b9ac9aff3e819bca75d3d2a4159
Douglass Turner <douglass.turner@gmail.com>
2013-05-27 07:55:10 -0400

    Xcode - Storyboard - wired up interface to viewController.

--- iOSCalculatorKataRoot/EIViewController.m
+++ iOSCalculatorKataRoot/EIViewController.m
@@ -9,7 +9,9 @@
 #import "EIViewController.h"
 
 @interface EIViewController ()
-
+@property (strong, nonatomic) IBOutlet UITextField *inputTextField;
+@property (strong, nonatomic) IBOutlet UITextField *outputTextField;
+- (IBAction)addNumbersButton:(id)sender;
 @end
 
 @implementation EIViewController
@@ -20,4 +22,6 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)addNumbersButton:(id)sender {
+}
 @end
