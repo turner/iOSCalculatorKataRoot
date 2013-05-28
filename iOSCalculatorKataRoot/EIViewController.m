0cb979ddd68929daaaa9b9caea9d87e14eec0790
Douglass Turner <douglass.turner@gmail.com>
2013-05-28 09:17:39 -0400

    Xcode - Calculator wired up to ViewController and UI.

--- iOSCalculatorKataRoot/EIViewController.m
+++ iOSCalculatorKataRoot/EIViewController.m
@@ -7,12 +7,12 @@
 //
 
 #import "EIViewController.h"
+#import "Calculator.h"
 
 @interface EIViewController ()
 @property (strong, nonatomic) IBOutlet UITextField *inputTextField;
 @property (strong, nonatomic) IBOutlet UITextField *outputTextField;
 - (IBAction)addButton:(id)sender;
-
 @end
 
 @implementation EIViewController
@@ -23,5 +23,18 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 - (IBAction)addButton:(id)sender {
+
+    @try {
+        Calculator *calculator = [[Calculator alloc] init];
+
+        int sum = [calculator add:self.inputTextField.text];
+        self.outputTextField.text = [NSString stringWithFormat:@"%d", sum];
+
+    }
+
+    @catch(NSException *e) {
+        self.outputTextField.text = [NSString stringWithFormat:@"%@", e.name];
+    }
+
 }
 @end
