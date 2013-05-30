505b560c95d0444cb763e1a59a71addf144aa095
Douglass Turner <douglass.turner@gmail.com>
2013-05-30 08:06:56 -0400

    Unit tests passed: test_GivenCalculator_WhenInputIsEmptyString_ThenReturnZero and test_GivenCalculator_WhenInputIsOneNumber_ThenReturnNumber

---
+++ iOSCalculatorKataRootTests/CalculatorTests.m
@@ -0,0 +1,62 @@
+#import <SenTestingKit/SenTestingKit.h>
+#import "Calculator.h"
+
+@interface CalculatorTests : SenTestCase
+
+@end
+
+@implementation CalculatorTests {
+    Calculator *_sut;
+}
+
+-(void)setUp {
+    [super setUp];
+    _sut = [[Calculator alloc] init];
+}
+
+-(void)testUnitTestRecipe {
+
+    // setup
+
+    // set expectations
+
+    // perform test
+
+    // verify test
+
+    STAssertEquals(YES, YES, nil);
+}
+
+-(void)test_GivenCalculator_WhenInputIsEmptyString_ThenReturnZero {
+
+    // setup
+    NSString *numbers = @"";
+
+    // set expectations
+    int expectation = 0;
+
+    // perform test
+    int sum = [_sut add:numbers];
+
+    // verify test
+    STAssertEquals(sum, expectation, nil);
+
+}
+
+-(void)test_GivenCalculator_WhenInputIsOneNumber_ThenReturnNumber {
+
+    // setup
+    NSString *numbers = @"5";
+
+    // set expectations
+    int expectation = 5;
+
+    // perform test
+    int sum = [_sut add:numbers];
+
+    // verify test
+    STAssertEquals(sum, expectation, nil);
+
+}
+
+@end
