0a2d2ca38c38832bc37ff75a55aa1cee32d0ee2f
Douglass Turner <douglass.turner@gmail.com>
2013-06-01 07:35:00 -0400

    Unit tests pass: test_GivenSUT_WhenInputIsEmptyString_ThenReturnZero and test_GivenSUT_WhenInputIsNumber_ThenReturnNumber

---
+++ iOSCalculatorKataRoot/Calculator.m
@@ -0,0 +1,18 @@
+//
+// Created by Douglass Turner on 6/1/13.
+// Copyright (c) 2013 Elastic Image Software. All rights reserved.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "Calculator.h"
+
+
+@implementation Calculator {
+
+}
+- (int)add:(NSString *)numbers {
+    return [numbers length] > 0 ? [numbers intValue] : 0;
+}
+@end
